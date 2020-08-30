cfe3feb2afed45868ca6aa3220c5debdf3f8d819
elsheikh21 <ahmedhelsheikh@gmail.com>
2020-08-30 18:33:03 +0000

    GC in setup.py & adding requirement

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@
 # NOTE: DO NOT declare anything here that you want to use in your stud folder!
 import nltk
 from transformers import AutoTokenizer, AutoModel
+import gc
+
 
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
@@ -12,3 +14,5 @@ nltk.download('stopwords', quiet=True)
 model_name = "xlm-roberta-large"
 tokenizer = AutoTokenizer.from_pretrained(model_name, do_lower_case=True)
 model = AutoModel.from_pretrained(model_name)
+del model, tokenizer
+gc.collect()
